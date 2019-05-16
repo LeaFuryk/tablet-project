551792622160182c40ce067e57bd65b3ac2f63d8
LFuryk <leandro.furyk@mercadolibre.com>
2019-05-16 20:17:11 -0300

    functional server

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ var express = require('express')
 var app = express()
 var path = require('path');
 
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+  res.sendFile('index.html');}
+);
 
- 
 app.listen(3000, () => console.error("Server listening on port 3000"));
